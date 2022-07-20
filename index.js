c5d4d5780ff638e9a53bbe0aa1b7938ee2f96689
Parker Parham <parkerparham@gmail.com>
2022-07-20 10:31:21 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+//
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'; //use no declaration when you need it to be global to whole program
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'mary';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'maybe mary';
+}
